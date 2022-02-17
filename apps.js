ded4fbf339c3c87809a7017bbdd183fe67b1bdb5
Rafsan Ahmed Raj <rafsan.web.2022@gmail.com>
2022-02-17 20:07:32 +0600

    final-js

--- apps.js
+++ apps.js
@@ -1,36 +1,117 @@
-function calcInput() {
-    const incomeInput = document.getElementById('income-input').value;
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothInput = document.getElementById('cloth-input').value;
-    let totalExpenses = document.getElementById('total-exp');
-    let balance = document.getElementById('balance');
-    total = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput) ;
-    totalExpenses.innerText = total;
-    remaining = parseFloat(incomeInput) - total;
-    balance.innerText = remaining;
+function getItem(id) {
+    const element = document.getElementById(id);
+    return element;
 }
 
-document.getElementById('calc-btn').addEventListener('click', function () {
-    calcInput();
+// All the Input Element
+const incomeInput = getItem('income');
+const foodInput = getItem('food');
+const rentInput = getItem('rent');
+const clothInput = getItem('clothes');
+const saveInput = getItem('save');
+
+// All the Output Element
+const totalExpenses = getItem('total-expenses');
+const balance = getItem('balance');
+const savingAmount = getItem('saving-amount');
+const remainBalance = getItem('remain-balance');
+
+//Error Output Element
+const incomeErr = getItem('income-err');
+const foodErr = getItem('food-err');
+const rentErr = getItem('rent-err');
+const clothErr = getItem('cloth-err');
+const savingErr = getItem('saving-err');
+
+// All the Button Element
+const calculateExpense = getItem('calculate');
+const calculateSaving = getItem('save-calculate');
+
+// Handle Expenses Calculation
+calculateExpense.addEventListener('click', function () {
+    const incomeAmount = parseFloat(incomeInput.value);
+    const foodAmount = parseFloat(foodInput.value);
+    const rentAmount = parseFloat(rentInput.value);
+    const clothAmount = parseFloat(clothInput.value);
+
+    // Invalid Input Error Handling
+    if (incomeAmount <= 0 || isNaN(incomeAmount)) {
+        incomeErr.style.display = 'block';
+        return;
+    } else {
+        incomeErr.style.display = 'none';
+    }
+
+    if (foodAmount <= 0 || isNaN(foodAmount)) {
+        foodErr.style.display = 'block';
+        return;
+    } else {
+        foodErr.style.display = 'none';
+    }
+
+    if (rentAmount <= 0 || isNaN(rentAmount)) {
+        rentErr.style.display = 'block';
+        return;
+    } else {
+        rentErr.style.display = 'none';
+    }
+
+    if (clothAmount <= 0 || isNaN(clothAmount)) {
+        clothErr.style.display = 'block';
+        return;
+    } else {
+        clothErr.style.display = 'none';
+    }
+
+    // Total Expense Calculation and Result Output
+    const expensesResult = foodAmount + rentAmount + clothAmount;
+    totalExpenses.innerText = expensesResult;
+
+    // Rest Balance and Output
+    if (expensesResult <= incomeAmount) {
+        const restBalance = incomeAmount - expensesResult;
+        balance.innerText = restBalance;
+        balance.style.color = '#232323';
+    } else {
+        balance.innerText = 'Less than Expenses!';
+        balance.style.color = 'red';
+    }
 });
 
+// Handle Saving Calculation
+calculateSaving.addEventListener('click', function () {
+    const currentBalance = balance.innerText;
+    const incomeAmount = parseFloat(incomeInput.value);
+    const savingRate = parseFloat(saveInput.value);
+    let savingResult = 0;
 
-// calculate save money part
-function saveMoney() {
-    const incomeInput = document.getElementById('income-input');
-    let incomeInputValue = incomeInput.value;
-    let balance = document.getElementById('balance').innerText;
-    let remainingBalanceNumber = document.getElementById('remaining-balance');
-    const saveMoneyInput = document.getElementById('save-input').value;
-    const savingAmount = document.getElementById('saving-amount');
-    let savesMoney = (incomeInputValue * saveMoneyInput) / 100;
-    remainingBalance = parseFloat(balance) - savesMoney;
-    savingAmount.innerText = savesMoney;
-    remainingBalanceNumber.innerText =  remainingBalance;
-}
+    // Saving Amount Calculation
+    if (incomeAmount <= 0 || isNaN(incomeAmount)) {
+        savingErr.style.display = 'block';
+        return;
+    } else {
+        savingResult += (incomeAmount / 100) * savingRate;
+        savingErr.style.display = 'none';
+    }
 
-document.getElementById('save-btn').addEventListener('click',function(){
-    saveMoney();
-})
-    
+    // Show Saving Amount Output
+    if (currentBalance >= savingResult && savingResult >= 0) {
+        savingAmount.innerText = savingResult;
+        savingErr.style.display = 'none';
+    } else {
+        savingAmount.innerText = 00;
+        remainBalance.innerText = 00;
+        savingErr.style.display = 'block';
+        return;
+    }
+
+    // Remaining Balance Calculation
+    if (currentBalance >= 0 || !isNaN(currentBalance) && savingResult >= 0) {
+        const remainBalanceResult = currentBalance - savingResult;
+        remainBalance.innerText = remainBalanceResult;
+        savingErr.style.display = 'none';
+    } else {
+        remainBalance.innerText = 00;
+        savingErr.style.display = 'block';
+    }
+});
